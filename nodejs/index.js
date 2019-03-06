ff861c5932a9c2f3e78b4df0eae528ee030698cd
Justin Brulotte <justin.brlotte797@gmail.com>
2019-03-06 13:51:53 -0500

    Minimal NodeJs Function

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,13 +1,10 @@
-var express = require('express')
-var app = express()
+var sleep = require('sleep');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+functionName()
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+function functionName() {
+    sleep.sleep(60);
+    console.log("Hello World");
+    sleep.sleep(60);
+    console.log("Good Bye");
+}
