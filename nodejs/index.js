6b7276ed195ed02bde43235cda531910b985b6f5
Justin Brulotte <justin.brlotte797@gmail.com>
2019-03-08 15:02:06 -0500

    Change NodeJs fonction

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,8 +3,10 @@ var sleep = require('sleep');
 functionName()
 
 function functionName() {
-    sleep.sleep(60);
     console.log("Hello World");
-    sleep.sleep(60);
+    var randomeNumber1 = Math.floor(Math.random() * 100)) + 1;
+    var randomeNumber2 = Math.floor(Math.random() * 100)) + 1;
+    var totalNumber = randomeNumber1 + randomeNumber2;
+    console.log("Doing " + randomeNumber1 + " + " + randomeNumber2 + " = " + totalNumber);
     console.log("Good Bye");
 }
