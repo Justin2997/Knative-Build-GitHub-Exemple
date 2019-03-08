64ca6e9c9bec25fffc73f2df2ec40dba2b82bcba
Justin Brulotte <justin.brlotte797@gmail.com>
2019-03-08 15:07:04 -0500

    Adding Int Random

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,10 +2,14 @@ var sleep = require('sleep');
 
 functionName()
 
+function randomIntInc(low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low)
+  }
+
 function functionName() {
     console.log("Hello World");
-    var randomeNumber1 = Math.floor(Math.random() * 100)) + 1;
-    var randomeNumber2 = Math.floor(Math.random() * 100)) + 1;
+    var randomeNumber1 = randomIntInc(0,10)
+    var randomeNumber2 = randomIntInc(0,10)
     var totalNumber = randomeNumber1 + randomeNumber2;
     console.log("Doing " + randomeNumber1 + " + " + randomeNumber2 + " = " + totalNumber);
     console.log("Good Bye");
