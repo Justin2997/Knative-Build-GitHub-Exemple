d3813aa890962c8be1fdd7644ac602cd76c985c7
Justin Brulotte <justin.brlotte797@gmail.com>
2019-04-16 09:08:34 -0400

    Change code to try riff

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,16 +1,5 @@
-var sleep = require('sleep');
-
-functionName()
-
-function randomIntInc(low, high) {
-    return Math.floor(Math.random() * (high - low + 1) + low)
-  }
-
-function functionName() {
-    console.log("Hello World");
-    var randomeNumber1 = randomIntInc(0,10)
-    var randomeNumber2 = randomIntInc(0,10)
-    var totalNumber = randomeNumber1 + randomeNumber2;
-    console.log("Doing " + randomeNumber1 + " + " + randomeNumber2 + " = " + totalNumber);
-    console.log("Good Bye");
+module.exports = x => {
+  const xx = x ** 2;
+  console.log(`the square of ${x} is ${xx}`);
+  return xx;
 }
